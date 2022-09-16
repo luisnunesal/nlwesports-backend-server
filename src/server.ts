1158082176cbac09c6c3dd3c584c500458ea1863
Luis Nunes <luis.nunes@captalys.com.br>
2022-09-16 16:18:32 -0300

    resolvendo o banco de dados com o prisma

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,33 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-//comentario teste ve se sobe no github
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
 
-app.get('/ads', (request, response) => {
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+
+app.get('/games/:id/ads', (request, response) => {
+    
+    const gameId = request.params.id;
+
     return response.json([
         { id: 1, name: 'Anuncio 1'},
         { id: 2, name: 'Anuncio 2'},
         { id: 3, name: 'Anuncio 3'},
         { id: 4, name: 'Anuncio 4'},
     ])
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+    
+    const adId = request.params.id;
+    return response.json([]);
+
 });
 
 app.listen(3333)
