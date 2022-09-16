34fdc400b47c324301b5368b27cdd04b11d82228
Luis Nunes <luis.nunes@captalys.com.br>
2022-09-16 13:56:08 -0300

    teste comentario pc captalys

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+//comentario teste ve se sobe no github
+
 app.get('/ads', (request, response) => {
     return response.json([
         { id: 1, name: 'Anuncio 1'},
